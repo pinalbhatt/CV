8c76cb3a766c6f3a85b7ef07d63a2f8043568b5d
Pinal Bhatt <Pinal.Bhatt@weightwatchers.com>
2015-10-16 21:09:56 -0400

    commit

--- assets/ngApp/RApp.controller.js
+++ assets/ngApp/RApp.controller.js
@@ -5,20 +5,60 @@
         .module('PBDesk.RApp')
         .controller('mainController', mainController);
 
-    mainController.$inject = ['$scope', '$rootScope'];
+    mainController.$inject = ['$scope', '$rootScope', '$cookies', 'APIService'];
 
-    function mainController($scope, $rootScope) {
+    function mainController($scope, $rootScope, $cookies, APIService) {
 
-        $scope.accessCode = "";
-        $scope.accessGranted = false;
-        $scope.title = 'mainController';
+
 
         $scope.requestAccess = function(){
-            $scope.accessGranted = !$scope.accessGranted ;
+            verifyAccessCode($scope.data.accessCode);
         }
 
         init();
 
-        function init() { }
+        function init() {
+            $scope.data = {accessCode: 1 };
+            $scope.accessGranted = false;
+            $scope.title = 'mainController';
+            activateRSvc();
+
+        }
+
+        function activateRSvc(){
+            var activateCookieName = "act"
+            var a = $cookies.get(activateCookieName);
+            if(a === undefined) {
+                var d = new Date();
+                d.setMinutes(d.getMinutes() + 15);
+                APIService.get("Activate").then(function (data) {
+                    console.log(data);
+                    $cookies.put(activateCookieName, "1", {'expires': d});
+                }, function (error) {
+                    console.log(error);
+                    $cookies.put(activateCookieName, "0", {'expires': d});
+                });
+            }
+        }
+
+        function verifyAccessCode(code){
+            if(code !== undefined && isFinite(code)){
+                var accessCodeCookieName = "ac";
+                var d = new Date();
+                d.setMinutes(d.getMinutes() + 30);
+                APIService.get("Access?a="+code).then(function (data) {
+                    console.log(data);
+                    if(data){
+                        cvData = data;
+                        $scope.accessGranted = true;
+                        $cookies.put(accessCodeCookieName, code, {'expires': d});
+                    }
+
+                }, function (error) {
+                    console.log(error);
+                });
+            }
+
+        }
     }
 })();
